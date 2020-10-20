db7230160476ac15c32dc0ac7c6db2c573ace4e0
ashleymader <ashley.mader12@gmail.com>
2020-10-20 13:51:28 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    
+
+    def new 
+
+    end
+
+    def create 
+        @user = User.find_by(name: params[:user][:name])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else  
+            redirect_to new_user_path
+        end
+    end
+    
+
+    private 
+
+    def user_params
+
+    end
+end
